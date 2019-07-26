c9910b0bb0770f94895b1889b67d6a9d72a4e5f7
朱海文 <zhuhaiwen@microduino.cc>
2019-07-26 14:52:23 +0800

    尝试publish

--- ios/Classes/FlutterIflyPlugin.m
+++ ios/Classes/FlutterIflyPlugin.m
@@ -12,7 +12,13 @@
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
+  } else if([@"speak" isEqualToString:call.method]){
+    //朗读
+    NSLog(@"朗读。 。 。 。 。 。");
+  }else if([@"" isEqualToString:call.method]){
+    //语音识别
+    NSLog(@"语音识别。 。 。 。 。 。");
+  }else{
     result(FlutterMethodNotImplemented);
   }
 }
